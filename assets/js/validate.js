9f69a27114cc3a00a55005af3fac0c258ddaa53f
miriam04 <maagm@bzz.ch>
2021-07-04 14:58:23 +0200

    asdf

--- assets/js/validate.js
+++ assets/js/validate.js
@@ -1,52 +1,13 @@
-/*
-	Caminar by TEMPLATED
-	templated.co @templatedco
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+/* Aufgabe:
+  Fügen Sie die notwendigen Selektoren für
+  firstname, lastname, mobile, password2
 */
 
-(function($) {
-
-	skel.breakpoints({
-		xlarge: '(max-width: 1680px)',
-		large: '(max-width: 1280px)',
-		medium: '(max-width: 980px)',
-		small: '(max-width: 736px)',
-		xsmall: '(max-width: 480px)'
-	});
-
-	$(function() {
-
-		var	$window 	= $(window),
-			$body 		= $('body'),
-			$header 	= $('#header');
-
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
-
-			$window.on('load', function() {
-				window.setTimeout(function() {
-					$body.removeClass('is-loading');
-				}, 100);
-			});
-
-		// Prioritize "important" elements on medium.
-			skel.on('+medium -medium', function() {
-				$.prioritize(
-					'.important\\28 medium\\29',
-					skel.breakpoint('medium').active
-				);
-			});
-
-		// Gallery.
-			$('.gallery').poptrox();
-
-	});
-
-	// read form element
 const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
+const birthday = document.getElementById('birthday');
 
 // Show input error message
 function showError(input, message) {
@@ -67,10 +28,19 @@ function checkEmail(input) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (re.test(input.value.trim())) {
     showSuccess(input);
+    return true
   } else {
-    showError(input, 'Email is not valid');
+    showError(input, 'Email ist ungültig');
   }
 }
+
+/* Aufgabe:
+    Validieren Sie die Mobile-Nummer ähnlich wie bei der Email mit einer
+    Regular expression (regex). Für eine geeignete regex suchen Sie
+    im Internet nach "javascript regular expression for mobile number".
+*/
+// Check phone is valid
+
 
 // Check required fields
 function checkRequired(inputArr) {
@@ -90,29 +60,59 @@ function checkRequired(inputArr) {
 // Check input length
 function checkLength(input, min, max) {
   if (input.value.length < min) {
-    showError(input,
+    showError(
+        input,
         `${getFieldName(input)} must be at least ${min} characters`
     );
   } else if (input.value.length > max) {
-    showError(input,
+    showError(
+        input,
         `${getFieldName(input)} must be less than ${max} characters`
     );
   } else {
     showSuccess(input);
+    return true;
   }
 }
+
+/* Aufgabe:
+    Validieren Sie, ob die beiden Passwörter übereinstimmen.
+    Falls sie nicht übereinstimmen, geben Sie (ähnlich wie in den anderen Beispielen)
+    eine Fehlermeldung dem Formular aus.
+*/
+// Check passwords match
 
 // Get fieldname
 function getFieldName(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
-// Validate form input elements
+function handleSuccess() {
+    form.innerHTML = '<h1>Vielen Dank fürs ausfüllen!</h1>'
+}
+
 function validateForm(){
-  if(!checkRequired([username, email, password])){
-    checkLength(username, 3, 15);
-    checkLength(password, 6, 25);
-    checkEmail(email);
+  if(!checkRequired([username, email, password, birthday])){
+      let statusList = []
+    //Aufgabe: Validierung der Länge für Vorname (2 bis 20) und Nachname (2 bis 50)
+    statusList.push(checkLength(username, 3, 15));
+    statusList.push(checkLength(password, 6, 25));
+    /* Aufgabe:
+      Validierung der Telefonnumer ähnlich wie bei der Email mit einer
+      Regular expression (regex). Für eine geeignete regex suchen Sie
+      im Internet nach "javascript regular expression for mobile number"
+    * */
+      statusList.push(checkEmail(email));
+    /* Aufgabe:
+      Validierung Sie die beiden Passwörter, damit password
+      mit password2 übereinstimmt.
+    * */
+
+      statusList = statusList.filter(x => x !== true)
+
+      if(statusList.length == 0) {
+          handleSuccess()
+      }
   }
 }
 
@@ -124,5 +124,3 @@ form.addEventListener('submit', function(e) {
   //First validate form
   validateForm();
 });
-
-})(jQuery);
