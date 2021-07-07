91b8feeabad3de08f24881600ccb3ecc350cd5df
miriam04 <maagm@bzz.ch>
2021-07-07 14:46:46 +0200

    commit

--- public/assets/js/validate.js
+++ public/assets/js/validate.js
@@ -1,15 +1,13 @@
-/* Aufgabe:
-  Fügen Sie die notwendigen Selektoren für
-  firstname, lastname, mobile, password2
+/* Formulartypen ID
 */
 
 const form = document.getElementById('form');
-const username = document.getElementById('username');
+const nachname = document.getElementById('nachname');
 const email = document.getElementById('email');
-const password = document.getElementById('password');
-const birthday = document.getElementById('birthday');
+const passwort = document.getElementById('passwort');
+const datum = document.getElementById('datum');
 
-// Show input error message
+// Fehlermeldung 
 function showError(input, message) {
   const formControl = input.parentElement;
   formControl.className = 'form-control error';
@@ -17,13 +15,13 @@ function showError(input, message) {
   small.innerText = message;
 }
 
-// Show success outline
+// Rahmen grün
 function showSuccess(input) {
   const formControl = input.parentElement;
   formControl.className = 'form-control success';
 }
 
-// Check email is valid
+// E-Mailadresse Gültigkeit überprüfen
 function checkEmail(input) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (re.test(input.value.trim())) {
@@ -34,20 +32,12 @@ function checkEmail(input) {
   }
 }
 
-/* Aufgabe:
-    Validieren Sie die Mobile-Nummer ähnlich wie bei der Email mit einer
-    Regular expression (regex). Für eine geeignete regex suchen Sie
-    im Internet nach "javascript regular expression for mobile number".
-*/
-// Check phone is valid
-
-
-// Check required fields
+// notwendige Felder überprüfen
 function checkRequired(inputArr) {
   let isRequired = false;
   inputArr.forEach(function(input) {
     if (input.value.trim() === '') {
-      showError(input, `${getFieldName(input)} is required`);
+      showError(input, `${getFieldName(input)} ist erforderlich`);
       isRequired = true;
     } else {
       showSuccess(input);
@@ -57,17 +47,17 @@ function checkRequired(inputArr) {
   return isRequired;
 }
 
-// Check input length
+// Zeichenlänge überprüfen
 function checkLength(input, min, max) {
   if (input.value.length < min) {
     showError(
         input,
-        `${getFieldName(input)} must be at least ${min} characters`
+        `${getFieldName(input)} muss mindestens ${min} Zeichen haben`
     );
   } else if (input.value.length > max) {
     showError(
         input,
-        `${getFieldName(input)} must be less than ${max} characters`
+        `${getFieldName(input)} muss weniger als ${max} Zeichen haben`
     );
   } else {
     showSuccess(input);
@@ -75,39 +65,24 @@ function checkLength(input, min, max) {
   }
 }
 
-/* Aufgabe:
-    Validieren Sie, ob die beiden Passwörter übereinstimmen.
-    Falls sie nicht übereinstimmen, geben Sie (ähnlich wie in den anderen Beispielen)
-    eine Fehlermeldung dem Formular aus.
-*/
-// Check passwords match
-
-// Get fieldname
+// Funktion Feldnamen automatisch übernehmen
 function getFieldName(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
 function handleSuccess() {
-    form.innerHTML = '<h1>Vielen Dank fürs ausfüllen!</h1>'
+    form.innerHTML = '<h1>Vielen Dank für Ihre Tischreservation!</h1>'
 }
 
 function validateForm(){
-  if(!checkRequired([username, email, password, birthday])){
+  if(!checkRequired([nachname, email, passwort, datum])){
       let statusList = []
-    //Aufgabe: Validierung der Länge für Vorname (2 bis 20) und Nachname (2 bis 50)
-    statusList.push(checkLength(username, 3, 15));
-    statusList.push(checkLength(password, 6, 25));
-    /* Aufgabe:
-      Validierung der Telefonnumer ähnlich wie bei der Email mit einer
-      Regular expression (regex). Für eine geeignete regex suchen Sie
-      im Internet nach "javascript regular expression for mobile number"
-    * */
-      statusList.push(checkEmail(email));
-    /* Aufgabe:
-      Validierung Sie die beiden Passwörter, damit password
-      mit password2 übereinstimmt.
-    * */
+    //Validierung Zeichenlänge
+    statusList.push(checkLength(nachname, 3, 15));
+    statusList.push(checkLength(passwort, 6, 25));
+    statusList.push(checkEmail(email));
 
+  //Erfolgsmeldung
       statusList = statusList.filter(x => x !== true)
 
       if(statusList.length == 0) {
@@ -116,8 +91,6 @@ function validateForm(){
   }
 }
 
-
-// Event listeners
 form.addEventListener('submit', function(e) {
   //https://www.w3schools.com/jsref/event_preventdefault.asp
   e.preventDefault();
